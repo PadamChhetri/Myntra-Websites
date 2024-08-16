2df8b4e30a492c7bb4370ecf90ccd6c949d407b7
Padam Chhetri <padamchhetri710@gmail.com>
2024-08-16 23:09:53 +0545

    Final Code

--- Mynta Functional Code/Scripts/index.js
+++ Mynta Functional Code/Scripts/index.js
@@ -1,4 +1,37 @@
+let bagItems;
+onLoad();
+
+function onLoad(){
+  let bagItemsStr=localStorage.getItem('bagItems');
+  bagItems=bagItemsStr ? JSON.parse(bagItemsStr):[];
+displayItemOnHomePage();
+displayBagIcon();
+}
+
+
+function addToBag(itemId){
+bagItems.push(itemId);
+localStorage.setItem('bagItems',JSON.stringify(bagItems));
+displayBagIcon();
+}
+
+function displayBagIcon(){
+  let bagItemCountElement=document.querySelector('.bag-item-count');
+  if(bagItems.length >0){
+    bagItemCountElement.style.visibility='visible';
+  bagItemCountElement.innerText=bagItems.length;
+  }
+  else{
+    bagItemCountElement.style.visibility='hidden';
+  }
+}
+
+function displayItemOnHomePage(){
 let itemsContainerElement= document.querySelector('.items-container');
+
+if(!itemsContainerElement){
+  return;
+}
 
 let innerHTML='';
 items.forEach(item =>{
@@ -15,8 +48,9 @@ items.forEach(item =>{
       <span class="original-price">RS ${item.original_price}</span>
       <span class="discount"> ${item.discount_percentage}% Off</span>
   </div>
-  <button class="btn-add-bag">Add to Bag</button>
+  <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
 </div>`
 });
  
 itemsContainerElement.innerHTML=innerHTML;
+}
