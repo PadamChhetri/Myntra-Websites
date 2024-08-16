9771b74f42dc1ad268f01376d99213f18d07ce5e
Padam Chhetri <padamchhetri710@gmail.com>
2024-08-16 20:17:53 +0545

    all item is placed

---
+++ Mynta Functional Code/Scripts/index.js
@@ -0,0 +1,22 @@
+let itemsContainerElement= document.querySelector('.items-container');
+
+let innerHTML='';
+items.forEach(item =>{
+  innerHTML+= `<div class="item-container">
+  <img class="item-image" src="${item.image}" alt="item image">
+  <div class="rating">
+  ${item.rating.stars}⭐ |${item.rating.count
+  }
+  </div>
+  <div class="company-name">  ${item.company}</div>
+  <div class="item-name">${item.item_name}</div>
+  <div class="price">
+      <span class="current-Price">RS ${item.current_price}</span>
+      <span class="original-price">RS ${item.original_price}</span>
+      <span class="discount"> ${item.discount_percentage}% Off</span>
+  </div>
+  <button class="btn-add-bag">Add to Bag</button>
+</div>`
+});
+ 
+itemsContainerElement.innerHTML=innerHTML;
